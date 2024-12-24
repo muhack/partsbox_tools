120ebafb1478254daf8d2c0d200161d42e47309c
Federico Cerutti <federico@ceres-c.it>
2024-12-24 18:02:59 +0100

    (Hopefully) working daemon + labels templates

--- daemon/partsbox_print_daemon.py
+++ daemon/partsbox_print_daemon.py
@@ -5,9 +5,60 @@ from functools import reduce
 import os
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from pathlib import Path
 import re
+import subprocess
+import tempfile
 
 import requests
+
+GLABELS_BIN = 'glabels-3-batch'
+GLABELS_PART_TEMPLATE = 'parts_template.glabels'
+GLABELS_PART_TEMPLATE_PATH = Path.cwd().parent / 'templates' / GLABELS_PART_TEMPLATE
+LPR_BIN = 'lpr'
+LPR_PRINTER = ''
+
+def print_data(parts_data: list[dict], glabel_template_file: str, printer_name: str = None): # type: ignore
+	'''
+	Generate a label and print it
+
+	:param param_data: Data to print
+	:param glabel_template_file: Path to the glabel template file
+	:param printer_name: Name of the printer to use (defaults to system default printer)
+	'''
+	# Create a temporary file to store the data
+	with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
+		print(f'[D] Writing data to {f.name}')
+		f.write(','.join(list(map(lambda x: f"\"{x}\"", parts_data[0].keys()))))
+		for part_data in parts_data:
+			f.write('\n')
+			f.write(','.join(list(map(lambda x: f"\"{x}\"", part_data.values()))))
+		f.flush()
+
+		# Generate the labels
+		print(f'[D] Generating labels to {f.name}.pdf')
+		glabels_args = [glabel_template_file, '-i', f.name, '-o', f'{f.name}.pdf']
+		try:
+			subprocess.run([GLABELS_BIN] + glabels_args, check=True, text=True, capture_output=True, timeout=10)
+		except subprocess.CalledProcessError as e:
+			print(f'[E] Could not generate labels: {e.stdout}, {e.stderr}')
+			return
+
+		# Print the labels
+		print(f'[D] Printing labels')
+		if printer_name is None:
+			lpr_args = [f'{f.name}.pdf']
+		else:
+			lpr_args = ['-P', printer_name, f'{f.name}.pdf']
+		try:
+			result = subprocess.run([LPR_BIN] + lpr_args, check=True, text=True, capture_output=True)
+		except subprocess.CalledProcessError as e:
+			print(f'[E] Could not print labels: {e.stdout}, {e.stderr}')
+			return
+
+		# Clean up
+		print(f'[D] Done, cleaning up')
+		os.remove(f'{f.name}.pdf')
 
 class PartsboxAPI:
 	'''
@@ -54,7 +105,7 @@ class PartsboxAPI:
 		'''
 		return part_data.get('part/stock', [{}])[0].get('stock/storage-id')
 
-	def get_part_total_stock(self, part_data: dict) -> int|None:
+	def get_part_total_stock(self, part_data: dict) -> int:
 		'''
 		Get the total stock of a part
 		'''
@@ -93,12 +144,12 @@ class PartsboxAPI:
 		storage_name = self.get_storage_name(storage_data)
 
 		return {
-			'Name': part_data.get('part/name'),
-			'Description': part_data.get('part/description'),
-			'Footprint': part_data.get('part/footprint'),
-			'Manufacturer': part_data.get('part/manufacturer'),
-			'MPN': part_data.get('part/mpn'),
-			'Storage': storage_name,
+			'Name': part_data.get('part/name', ''),
+			'Description': part_data.get('part/description', ''),
+			'Footprint': part_data.get('part/footprint', ''),
+			'Manufacturer': part_data.get('part/manufacturer', ''),
+			'MPN': part_data.get('part/mpn', ''),
+			'Storage': '' if storage_name is None else storage_name,
 			'Total Stock': self.get_part_total_stock(part_data),
 			'URL': PartsboxAPI.get_part_IdAnything_url(part_id),
 			'Meta-Parts': '', # Sorry, no easy way to get this data with the API, it seems...
@@ -151,6 +202,7 @@ class PartsboxPrinterReqHandler(BaseHTTPRequestHandler):
 		self.send_dict_response(response)
 
 		# Process the URLs
+		parts_csv_data = []
 		for url in data:
 			print(f'[D] Processing URL: {url}')
 			part_id = PartsboxAPI.get_part_id(url)
@@ -162,8 +214,9 @@ class PartsboxPrinterReqHandler(BaseHTTPRequestHandler):
 			if not part_csv_data:
 				print(f'[E] Could not get part data for ID: {part_id}. Aborting.')
 				continue
+			parts_csv_data.append(part_csv_data)
 
-			print(json.dumps(part_csv_data, indent=4))
+		print_data(parts_csv_data, GLABELS_PART_TEMPLATE_PATH.as_posix())
 
 PORT = 9581
 pa: PartsboxAPI = None # type: ignore
@@ -179,7 +232,6 @@ def main() -> int:
 	httpd = HTTPServer(('127.0.0.1', PORT), PartsboxPrinterReqHandler)
 	print(f'Hosting server on port {PORT}')
 	httpd.serve_forever()
-
 
 if __name__ == '__main__':
 	exit(main())
